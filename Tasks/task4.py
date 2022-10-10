d28953458287fed89204e5468726376c1ac51945
Dmitry <kochetov.dmitry@mail.ru>
2022-10-10 19:22:40 +0300

    исправлена ошибка

--- Tasks/task4.py
+++ Tasks/task4.py
@@ -1,6 +1,6 @@
 # 4.Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
 
-# *Пример:* 
+# *Пример:*
 
 # - k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 import random
@@ -15,7 +15,7 @@ k = int(input('Введите число: '))
 
 for i in range(k, 1, -1):
     stroka = stroka + str(koef[i]) + ' ' + 'x ^ ' + str(i) + ' + '
-stroka = stroka + str(koef[i+1]) + 'x ' str(koef[i+2])
+stroka = stroka + str(koef[i+1]) + 'x ' + '+ ' str(koef[i+2])
 
 with open('file_task4.txt', 'w') as f:
     f.write(stroka)
